566ad77d20ba72f1356611eebeabc555a4e734eb
Nikola Borisic <borisic.nikola@yahoo.com>
2021-05-05 23:18:21 +0200

    Finish functionality

--- src/week-four/index.jsx
+++ src/week-four/index.jsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState, useRef} from 'react';
 import Box from './box';
 import {cloneDeep} from 'lodash';
 
@@ -11,29 +12,10 @@ const blankBoxValues = [
     [0,0,0,0],
 ]
 
-const moveRight = (boxValues) => {
-        // const boxValuesCopy =  cloneDeep(boxValues);
-        // for(let i = 0; i < boxValuesCopy.length; i++) {
-        //     // debugger
-        //     let movedArray = moveArray(blankBoxValues[i], 'right');
-        //     let sumedArray = sumArray(movedArray);
-        //     let finalArray = moveArray(sumedArray, 'right');
-        //     boxValuesCopy[i] = finalArray;
-        // }
-        console.table(boxValues);
-        // setDirection('right');
-        // setBoxValues(boxValuesCopy);
-        // console.table([...boxValues]);
-    }
-
-    const arrowsMap = {
-        'ArrowRight': moveRight
-    }
-
-
 const NumbersChallenge = () => {
     const [boxValues, setBoxValues] = useState(blankBoxValues);
-    const [direction, setDirection] = useState(null);
+    const [gameEnd, setGameEnd] = useState(false);
+    const direction = useRef(null);
    
     const moveArray = (array, dir) => {
         const arrayCopy = cloneDeep(array);
@@ -42,7 +24,7 @@ const NumbersChallenge = () => {
         const zeroArr = Array(array.length - arrayLen).fill(0);
         let movedArray;
         
-        if(dir === 'right' || dir === 'up') {
+        if(dir === 'down' || dir === 'right') {
             movedArray = zeroArr.concat(arrayValues);
         } else {
             movedArray = arrayValues.concat(zeroArr);
@@ -50,45 +32,57 @@ const NumbersChallenge = () => {
 
         return movedArray;
     }
+    
+    const transValues = (values) => {
+        return values[0].map((_, colIndex) => values.map(row => row[colIndex]));
+    }
 
-    const sumArray = (array) => {
-        let arrayCopy = cloneDeep(array)
+    const checkGameEnd = (values) => {
+        for(let i = 0; i <values.length; i++) { 
+            for (let j = values[i].length; j > 0; j--) {
+                if(values[i][j] === values[i][j - 1]) {
+                    return false
+                }
+            }
+        }
+
+        const transValuesToCheck = transValues([...values]);
+        for(let i = 0; i <transValuesToCheck.length; i++) { 
+            for (let j = transValuesToCheck[i].length; j > 0; j--) {
+                if(transValuesToCheck[i][j] === transValuesToCheck[i][j - 1]) {
+                    return false
+                }
+            }
+        }
+
+        return true;
+    }
+
+    const sumArray = (array, dir) => {
+        let arrayCopy = cloneDeep(array);
+        const hasToFlip = dir === 'up' || dir === 'left';
+        if (hasToFlip) {arrayCopy.reverse()}
         for (let i = arrayCopy.length; i > 0; i--) {
             if(arrayCopy[i] === arrayCopy[i - 1]) {
                 arrayCopy[i] = arrayCopy[i] * 2;
                 arrayCopy[i - 1] = 0;
             }
         }
+        if (hasToFlip) {arrayCopy.reverse()}
         return arrayCopy;
-    }
-
-    const moveRight = (boxValues) => {
-    //     // const boxValuesCopy =  cloneDeep(boxValues);
-    //     // for(let i = 0; i < boxValuesCopy.length; i++) {
-    //     //     // debugger
-    //     //     let movedArray = moveArray(blankBoxValues[i], 'right');
-    //     //     let sumedArray = sumArray(movedArray);
-    //     //     let finalArray = moveArray(sumedArray, 'right');
-    //     //     boxValuesCopy[i] = finalArray;
-    //     // }
-        console.table(boxValues);
-    //     // setDirection('right');
-    //     // setBoxValues(boxValuesCopy);
-    //     // console.table([...boxValues]);
     }
 
     const createNumber = (options, values) => {
         const optionIndex = Math.floor(Math.random() * options.length);
         const {row, column} = options[optionIndex];
-        const randomNumberToAdd = Math.random() > 0.8 ? 4 : 2; 
+        const randomNumberToAdd = Math.random() > 0.85 ? 4 : 2; 
 
         values[row][column] = randomNumberToAdd;
         options.splice(optionIndex, 1);
     }
 
-    const generateRandomNumbers = () => {
+    const generateRandomNumbers = (boxValues) => {
         const options = [];
-        console.table(boxValues)
         const boxValuesCopy = cloneDeep(boxValues);
 
         for (let i = 0; i < boxValues.length; i++) {
@@ -103,58 +97,114 @@ const NumbersChallenge = () => {
         }
 
         const hasTwoOrMoreFreeSpace = options.length >= 2;
-        if(hasTwoOrMoreFreeSpace) {
+        if(hasTwoOrMoreFreeSpace && Math.random() < 0.7) {
             createNumber(options, boxValuesCopy);
             createNumber(options, boxValuesCopy);
-            console.log(boxValuesCopy);
-            setBoxValues(boxValuesCopy);
+        } else if(options.length) {
+            createNumber(options, boxValuesCopy);
         } else {
-            // createNumber(options, boxValuesCopy);
-            // setBoxValues(boxValuesCopy);
+            // hasMovesLeft
         }
-        
+
+        return boxValuesCopy;
     }
 
-    
+    const checkForChange = (prevValues, nextValues) => {
+        for (let i = 0; i < prevValues.length; i++) {
+            for (let j = 0; j < prevValues[i].length; j++) {
+                if(prevValues[i][j] !== nextValues[i][j]) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    const moveHorizontal = (dir) => {
+        setBoxValues((prevState) => {
+            const prevStateCopy = [...prevState];
+
+            for(let i = 0; i < prevStateCopy.length; i++) {
+                let movedArray = moveArray(prevStateCopy[i], dir);
+                let summedArray = sumArray(movedArray, dir);
+                let finalArray = moveArray(summedArray, dir);
+                prevStateCopy[i] = finalArray;
+            }
+            const changeHappened = checkForChange(prevStateCopy, prevState);
+
+            const newNumbers = generateRandomNumbers(prevStateCopy)
+            if(changeHappened && checkGameEnd(newNumbers)) {
+                setGameEnd(true)
+            }
+
+            return changeHappened ? newNumbers : prevState;
+        })
+    }
+
+    const moveVertical = (dir) => {
+        setBoxValues((prevState) => {
+            const prevStateCopy = [...prevState];
+            const tansValues = transValues(prevStateCopy);
+
+            for(let i = 0; i < tansValues.length; i++) {
+                let movedArray = moveArray(tansValues[i], dir);
+                let summedArray = sumArray(movedArray, dir);
+                let finalArray = moveArray(summedArray, dir);
+                tansValues[i] = finalArray;
+            }
+            const revertMatrix = transValues(tansValues);
+
+            const newNumbers = generateRandomNumbers(revertMatrix)
+            const changeHappened = checkForChange(revertMatrix, prevState);
+
+            if(changeHappened && checkGameEnd(newNumbers)) {
+                setGameEnd(true)
+            }
+            
+            return changeHappened ? newNumbers : prevState;
+        });
+    }
 
     const keyHandler = (e) => {
-        // arrowsMap[e.key](boxValues);
         switch (e.key) {
             case 'ArrowRight':
-                moveRight(boxValues);
+                moveHorizontal('right');
                 break;
-        //     case ' ':
-        //         generateRandomNumbers() 
-        //         break;
-        //     // case 'ArrowLeft':
-        //     //     moveRight();
-        //     //     break;
-        //     // case 'ArrowDone':
-        //     //     moveDown();
-        //     //     break;
-        //     // case 'ArrowUp':
-        //     //     moveUp();
-        //     //     break;
+            case 'ArrowLeft':
+                moveHorizontal('left');
+                break;
+            case 'ArrowUp':
+                moveVertical('up');
+                break;
+            case 'ArrowDown':
+                moveVertical('down');
+                break;
             default:
                 console.log('Use arrow keys');
         }
     }
 
-    useEffect(() => {
-        console.log('mount');
+    useEffect(() => {  
         document.addEventListener('keyup',keyHandler);
-        generateRandomNumbers()
 
         return () => {
-            console.log('unmount');
             document.removeEventListener('keyup',keyHandler)    
         }
+    }, [boxValues])
+
+    useEffect(() => {
+        const startingNumbers = generateRandomNumbers(boxValues);
+        setBoxValues(startingNumbers);
     }, [])
 
-    console.log(boxValues);
     return (
+        <div>
+        {gameEnd && <div>GAME END</div>}
         <div className='box-container'>
-            {boxValues.map((row, rowIndex) => row.map((value, colIndex) => <Box key={`${rowIndex}-${colIndex}`} direction={direction} boxValues={boxValues} value={value} row={rowIndex} col={colIndex} />))}
+            
+            {boxValues.map((row, rowIndex) => row.map((value, colIndex) => <Box key={`${rowIndex}-${colIndex}`} direction={direction.current} boxValues={boxValues} value={value} row={rowIndex} col={colIndex} />))}
+        </div>
         </div>
     )
 }
